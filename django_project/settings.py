ab5d9211791d68885bb14f958676ebe8b38ae6fa
Gourav Goyal <gouravgoyal290@gmail.com>
2019-09-23 14:05:55 +0530

    Updated Static Root

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -134,7 +135,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-YOUR_ACCESS_TOKEN = os.environ.get('YOUR_ACCESS_TOKEN')
-dbx = dropbox.Dropbox('YOUR_ACCESS_TOKEN')
